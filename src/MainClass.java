f883e207a5d1bd7a2ba6b18e978af3565c7625b9
aryanjagarwal <68804795+aryanjagarwal@users.noreply.github.com>
2022-02-12 11:56:53 +0530

    methods

--- src/MainClass.java
+++ src/MainClass.java
@@ -13,10 +13,24 @@ public class MainClass {
 
         System.out.println(p1.name + " " + p1.age);
         System.out.println(p2.name + " " + p2.age);
+
+        p1.eat();
+        p2.walk();
+        p2.walk(6);
     }
 }
 
 class person{
     String name;
     int age;
+
+    void walk() {
+        System.out.println(name + " is walking.");
+    }
+    void eat() {
+        System.out.println(name + " is eating.");
+    }
+    void walk(int steps) {
+        System.out.println(name + " walked " + steps);
+    }
 }
